dbb13eebade6a07a2f71aa08b97d776463ee5b8f
Pritam Makwana <pritammakwana17561@gmail.com>
2024-08-26 11:17:25 +0530

    upload

--- main.js
+++ main.js
@@ -4,7 +4,6 @@ function createWindow() {
     const win = new BrowserWindow({
         width: 800,
         height: 600,
-        frame: false,
         webPreferences: {
             nodeIntegration: true
         }
